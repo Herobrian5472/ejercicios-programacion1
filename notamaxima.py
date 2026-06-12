6fed2f3ba167e0ff0ecd46730902931365936832
Brian Gallardo <brianleonelg.95@gmail.com>
2026-06-11 23:59:36 -0300

    corte por control ventas (1er ej de corte) y notamaxima (sin controlar)

---
+++ notamaxima.py
@@ -0,0 +1,14 @@
+'''
+Alumnos por curso: calcular la cantidad de alumnos por curso
+
+Promedio total de alumnos y nota maxima a quien corresponde
+
+'''
+
+cursos    = ["1A", "1A", "1A", "1B", "1B", "1C", "1C", "1C", "1C", "2D"]
+
+nombres   = ["Ana", "Luis", "Juan", "Pedro", "Sofia", "Carlos", "Maria", "Jose", "Elena", "Raul"]
+
+notas     = [8, 7, 9, 6, 8, 10, 7, 9, 8, 6]
+        
+        
--- ventas.py
+++ ventas.py
@@ -4,9 +4,6 @@ ventas = 0
 total = 0
 vendedores = 0
 
-lista_vendedores = []
-lista_montos = []
-
 while vendedor != 99:
     
     vendedor_actual = vendedor
@@ -15,14 +12,11 @@ while vendedor != 99:
     
     while vendedor_actual == vendedor:
         
-        
         print("__________________")
         print("\nVendedor: ",vendedor_actual)
         venta = float(input("De cuánto fue la venta: $"))
         monto += venta
         movimientos += 1
-        lista_vendedores.append(vendedor)
-        lista_montos.append(monto)
         vendedor = int(input("\nIngrese al vendedor: "))
     
     print("===================")    
@@ -31,14 +25,14 @@ while vendedor != 99:
     print("Total: ",monto)
     print("===================")   
     
+    ventas += movimientos
+    total += monto
+    vendedores += 1
+    
 print("\n\n===================")
 print("Fin de las ventas.\n")    
 print(f'Se realizaron un total de {ventas} ventas')
 print(f'Por un monto total de ${total}')
 print(f'Entre {vendedores} vendedores')
 print("===================")  
-
-print(lista_vendedores)
-print(lista_montos)
-    
     
